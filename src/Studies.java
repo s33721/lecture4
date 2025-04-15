56845d9064a46a7de342c74ad5a5bdcbf7240130
s33721 <s33721@pjwstk.edu.pl>
2025-04-15 12:32:52 +0200

    before inheritance

---
+++ src/Studies.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Studies {
+    public String name;
+    public String description;
+
+    public ArrayList<Student> students;
+    public ArrayList<Teacher> teachers;
+    public ArrayList<Employee> employees;
+}
