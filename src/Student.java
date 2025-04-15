4d9dd89bafd7e98684f03e35166680a7e509c130
s33721 <s33721@pjwstk.edu.pl>
2025-04-15 12:28:37 +0200

    created student and teacher

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public String firstName;
+    public String lastName;
+    public int indexNumber;
+    public String studentEmail;
+
+    public void displayInfo() {
+        System.out.println("Student");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Student Email: " + studentEmail);
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,13 @@
+public class Teacher {
+    public String firstName;
+    public String lastName;
+    public int salary;
+    public String position;
+    public void displayInfo(){
+        System.out.println("Teacher");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Salary: " + salary);
+        System.out.println("Position: " + position);
+    }
+}
